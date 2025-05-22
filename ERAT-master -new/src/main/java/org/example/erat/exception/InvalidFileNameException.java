063c6eec201d749d64c142736d9cc5dc287ad901
Yiaaaaa <141634214+Yiaaaaa@users.noreply.github.com>
2025-05-22 23:28:24 +0800

    Delete ERAT-master -new directory

--- ERAT-master -new/src/main/java/org/example/erat/exception/InvalidFileNameException.java
+++
@@ -1,7 +0,0 @@
-package org.example.erat.exception;
-
-public class InvalidFileNameException extends Exception {
-    public InvalidFileNameException(String message) {
-        super(message);
-    }
-}
